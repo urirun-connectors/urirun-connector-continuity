750bf1ea666f36b41b30baf9c75ad8e4693d23f8
Tom Softreck <tom@sapletta.com>
2026-07-14 22:11:09 +0200

    chore: add project.sh/tree.sh dev scripts

---
+++ tree.sh
@@ -0,0 +1,4 @@
+# Author: Tom Sapletta · https://tom.sapletta.com
+# Part of the ifURI solution.
+
+tree -L 6 -I node_modules -I .venv -I venv -I c10 -I docs -I __pycache__ -I release -I playwright-report -I test-results -I dist -I _archive -I project -I build -I playwright > tree.txt
